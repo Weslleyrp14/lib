5f5f56df9a14c13c64a934d9192d830090a2949f
fabiojavaroni <54987728+fabiojavaroni@users.noreply.github.com>
2019-09-14 10:57:05 -0300

    Add files via upload

--- Sprites_VS_2.js
+++ Sprites_VS_2.js
@@ -6,8 +6,8 @@ function personagem(nome, x, y){
 function carregarPersonagens(){
 	fantasma = loadAnimation('https://raw.githubusercontent.com/fabiojavaroni/imagens/master/fantasma-1.png'); 
 	fantasma2 = loadAnimation('https://raw.githubusercontent.com/fabiojavaroni/imagens/master/fantasma-1.png', 'https://raw.githubusercontent.com/fabiojavaroni/imagens/master/fantasma-7.png');
-	estrela = loadAnimation('https://raw.githubusercontent.com/fabiojavaroni/imagens/master/estrela-normal-1.png');
-	estrela2 = loadAnimation('https://raw.githubusercontent.com/fabiojavaroni/imagens/master/estrela-normal-1.png', 'https://raw.githubusercontent.com/fabiojavaroni/imagens/master/estrela-normal-3.png');
+	estrela = loadAnimation('https://raw.githubusercontent.com/fabiojavaroni/imagens/master/estrelanormal1.png');
+	estrela2 = loadAnimation('https://raw.githubusercontent.com/fabiojavaroni/imagens/master/estrelanormal1.png', 'https://raw.githubusercontent.com/fabiojavaroni/imagens/master/estrelanormal3.png');
 	bolha = loadAnimation('https://raw.githubusercontent.com/fabiojavaroni/imagens/master/bolha-1.png');
 	bolha2 = loadAnimation('https://raw.githubusercontent.com/fabiojavaroni/imagens/master/bolha-1.png', 'https://raw.githubusercontent.com/fabiojavaroni/imagens/master/bolha-4.png');
 	sol = loadAnimation('https://raw.githubusercontent.com/fabiojavaroni/imagens/master/sol-1.png');
