6f6c3f15187199837721a650b74bd2d731cfbb4f
fabiojavaroni <54987728+fabiojavaroni@users.noreply.github.com>
2019-09-14 10:10:01 -0300

    Add files via upload

--- Sprites_VS_2.js
+++ Sprites_VS_2.js
@@ -4,10 +4,10 @@ function personagem(nome, x, y){
 
 
 function carregarPersonagens(){
-	var fantasma = loadAnimation('https://raw.githubusercontent.com/fabiojavaroni/imagens/master/fantasma-1.png'); 
-	var estrela = loadAnimation('https://raw.githubusercontent.com/fabiojavaroni/imagens/master/estrela-normal-1.png');
-	var bola = loadAnimation('https://raw.githubusercontent.com/fabiojavaroni/imagens/master/estrelaredonda9.png');
-	var sol = loadAnimation('https://raw.githubusercontent.com/fabiojavaroni/imagens/master/sol-1.png');
+	fantasma = loadAnimation('https://raw.githubusercontent.com/fabiojavaroni/imagens/master/fantasma-1.png'); 
+	estrela = loadAnimation('https://raw.githubusercontent.com/fabiojavaroni/imagens/master/estrela-normal-1.png');
+	bola = loadAnimation('https://raw.githubusercontent.com/fabiojavaroni/imagens/master/estrelaredonda9.png');
+	sol = loadAnimation('https://raw.githubusercontent.com/fabiojavaroni/imagens/master/sol-1.png');
 }
 
 
