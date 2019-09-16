f078505d10f1436665141ed959f1efdddcd0713d
fabiojavaroni <54987728+fabiojavaroni@users.noreply.github.com>
2019-09-16 11:05:49 -0300

    Add files via upload

--- Sprites_VS_3.js
+++ Sprites_VS_3.js
@@ -1,15 +1,15 @@
 function fantasma1(x, y){
   var fantasma1 = PegaImagem('fantasma-1');
   var fantasma2 = PegaImagem('fantasma-7');
-  fantasma = createSprite(x, y);
-  fantasma.addAnimation('normal', fantasma1, fantasma2);
+  fantasma1 = createSprite(x, y);
+  fantasma1.addAnimation('normal', fantasma1, fantasma2);
 }
 
 
 function monstro1(x, y){
   var monstro1 = PegaImagem('monstro');
-  monstro = createSprite(x, y);
-  monstro.addAnimation('normal', monstro1);
+  monstro1 = createSprite(x, y);
+  monstro1.addAnimation('normal', monstro1);
 }
 
 
